490098fad49b1395f3349cf93db0db0ea9f19aec
Alex Rosenstern <rexsatanas@yandex.ru>
2023-03-02 20:27:56 +0300

    repared

--- test1.py
+++ test1.py
@@ -1,8 +1,9 @@
 print('Hello World')
 print(' Friday tomorrow')
 print('wewewe')
-silcer = 13
-print(silver)
-sdfsdf
+silver = 13
+if silver == 13:
+    print(silver)
+else:
+    print(12)
 
-asdasd
